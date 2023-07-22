fa41a6952a872e747e1eedf00c793cd189a494a2
BernardoBF4 <bernardo.fantin@gmail.com>
2023-07-22 11:58:32 -0300

    feat(site: componente de formulário): criado já com a função de cronômetro

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,19 @@
     <div class="column is-one-quarter">
       <BarraLateral />
     </div>
-    <div class="column is-three-quarter"></div>
+    <div class="column is-three-quarter">
+      <TheFormulario />
+    </div>
   </main>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 import BarraLateral from './components/BarraLateral.vue'
+import TheFormulario from './components/TheFormulario.vue'
 
 export default defineComponent({
-  components: { BarraLateral },
+  components: { BarraLateral, TheFormulario },
   name: 'App',
 })
 </script>
