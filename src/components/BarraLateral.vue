893b2d4ca1a2b085ef1b89957f7c2ca4e156ec8f
BernardoBF4 <bernardo.fantin@gmail.com>
2023-07-22 11:21:19 -0300

    feat(site: home): primeiro componente criado

---
+++ src/components/BarraLateral.vue
@@ -0,0 +1,29 @@
+<template>
+  <header>
+    <h1>
+      <img src="../assets/logo.png" alt="" />
+    </h1>
+  </header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'BarraLateral',
+})
+</script>
+
+<style scoped>
+header {
+  background-color: #0d3b66;
+  height: 100vh;
+  width: 100%;
+}
+@media only screen and (max-width: 768px) {
+  header {
+    height: auto;
+    padding: 2.5rem;
+  }
+}
+</style>
